c6d180a81fdb0f08a4b3826be6a5b6510fd5497e
Supriyo Mahato <mesupriyomahato@gmail.com>
2025-04-22 23:17:27 +0530

    added menu

--- src/components/lander/quickspot.tsx
+++ src/components/lander/quickspot.tsx
@@ -165,7 +165,7 @@ const menuData = {
   },
   lunch: {
     text: "Lunch",
-    image: "https://plus.unsplash.com/premium_photo-1672976129906-5982aa060951?q=80&w=2488&auto=format&fit=crop",
+    image: "https://images.unsplash.com/photo-1626323107927-008ae2828ab6?q=80&w=2487&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
   },
   dinner: {
     text: "Dinner",
